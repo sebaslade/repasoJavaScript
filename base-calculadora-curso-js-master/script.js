6c3b2e9187d68a09c6f2cc4b4d3f5dbc77c07ecb
sebaslade <sebaslade29@gmail.com>
2024-01-25 22:16:36 -0500

    1 primer commit

---
+++ base-calculadora-curso-js-master/script.js
@@ -0,0 +1,16 @@
+//Agrega valores a la pantalla (input) mediante el .value
+function agregar(valor){
+    document.getElementById('pantalla').value += valor
+}
+
+//Borra los valores del input
+function borrar(){
+    document.getElementById('pantalla').value = ''
+}
+
+//Calcula el resultado
+function calcular(){
+    const valorPantalla = document.getElementById('pantalla').value
+    const resultado = eval(valorPantalla)
+    document.getElementById('pantalla').value = resultado
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+function mostrar(){
+    document.getElementById("seleccionable").innerHTML = Date()
+}
